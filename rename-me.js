97c6f923e448c4aa5131ef142d7040cbe75f50c9
Philip Ainsworth <painsworth@fullsail.edu>
2012-03-07 23:24:36 -0500

    initial git hub submit for project 2

--- rename-me.js
+++ rename-me.js
@@ -1,1 +1,42 @@
-alert("JavaScript works!");
+var logIt = function (input) {
+	if (!input) {
+		console.log("We have nothing to log.");
+	} else {
+		console.log(input);
+	};
+};
+
+var dragons = ["Red", "Blue", "Green", "RazorBack", "Gorgonthaller"]
+var dragonhp = [2000, 1000, 500, 250, 125]
+var wizards = ["Merlin", "Morgana", "Gandalf", "Dumbledore"]
+var wizardap = [500, 400, 200, 100]
+var char,
+	power;
+
+var battle = function(wiz, drag) {
+	var strike = drag
+	while (wiz < drag) {
+		strike =- wiz;
+		logIt("You hit the dragon! It's hit points have been reduced to " + strike);
+	};
+	return 1;
+}
+
+
+
+var stats = function (char, power) {
+		if (char===dragons) {
+			for (i=0; i<char.length;i++) {
+				logIt("The " + char[i] + " Dragon has " + power[i] + " hit points"); 
+			};
+		} else if (char===wizards) {
+			for (i=0; i<char.length;i++) {
+				logIt("The Wizard " + char[i] + " has " + power[i] + " attack points"); 
+			};
+		} else {
+			logIt("Why is this getting chosen?");
+		};
+};
+battle(wizardap[0], dragonhp[0]);
+stats(dragons, dragonhp);
+stats(wizards, wizardap);
